a85f186223af780d1464e3cdd3a3aab0d121db1b
Marc Tamlyn <marc.tamlyn@gmail.com>
2014-10-14 20:28:56 +0100

    Set STATIC_ROOT.

--- squads/settings.py
+++ squads/settings.py
@@ -58,6 +58,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 
 # Email
